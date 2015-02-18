21297ee2210bd3b950f30ea61325cf5e54e669e5
octopuscabbage <octopuscabbage@gmail.com>
2015-02-17 20:35:22 -0600

    Board and Main started

---
+++ src/Board.hs
@@ -0,0 +1,29 @@
+module Board where
+
+import Data.Matrix
+import qualified Data.Vector as V
+
+data Piece = Player | Opponent | Empty deriving (Eq,Show,Read)
+type Board = Matrix Piece
+
+--Board addressing is always row column
+
+blankBoard :: Board
+blankBoard = matrix 6 7 (\_ -> Empty) 
+
+--Detect if player has won
+detectWin:: Board  -> Piece-> Bool
+detectWin board player= undefined
+
+dropPiece:: Piece -> Int -> Board -> Board
+dropPiece piece column board = setElem piece (findLastEmptyRow, column) board
+	where 	findLastEmptyRow = V.foldl (\i element -> if element == Empty then i+1 else i) 0  $ getColumn 
+		getColumn = getCol column board
+
+dropPlayer ::  Int -> Board -> Board
+dropPlayer = dropPiece Player
+
+dropOpponent ::  Int -> Board -> Board
+dropOpponent = dropPiece Opponent
+
+
---
+++ src/Main.hs
@@ -0,0 +1,8 @@
+module Main where
+
+import Board
+
+main = print $ dropOpponent 6$dropPlayer 6 $ blankBoard
+
+
+
